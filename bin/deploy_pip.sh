9780ae64d99d795f00325396c9bad2ed5359ce48
Ariel Sosnovsky <ariel@sosnovsky.ca>
2018-10-12 18:23:11 -0400

    - rebuild docs - added build scripts

---
+++ bin/deploy_pip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Get working dir (this will make sure that the script will work the same anywhere you run it)
+WK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $WK_DIR
+cd ..
+
+python setup.py sdist bdist_wheel
+twine upload dist/*
