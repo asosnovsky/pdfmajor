76deefb9dc1c9076c121e74a3ad6bff81d61297d
Ariel Sosnovsky <ariel@sosnovsky.ca>
2018-10-28 23:52:59 -0400

    Deployong new version - v1.1.2

--- pdfmajor/__init__.py
+++ pdfmajor/__init__.py
@@ -75,7 +75,7 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-__version__ = (1, 1, 1)
+__version__ = (1, 1, 2)
 
 if __name__ == '__main__':
     print(
