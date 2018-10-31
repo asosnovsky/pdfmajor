0d832bf7fadd44883a28040c8cd79dd4906aa20d
Ariel Sosnovsky <ariel@sosnovsky.ca>
2018-10-31 14:45:37 -0400

    version inc

--- pdfmajor/__init__.py
+++ pdfmajor/__init__.py
@@ -75,7 +75,7 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-__version__ = (1, 1, 5)
+__version__ = (1, 1, 6)
 
 if __name__ == '__main__':
     print(
