d43b6173095c90af1cea414703432819d5958206
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-01-11 15:23:15 -0500

    - new doc build

--- pdfmajor/__init__.py
+++ pdfmajor/__init__.py
@@ -97,7 +97,7 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-__version__ = (1, 3, 2b)
+__version__ = (1, 3, 2c)
 
 if __name__ == '__main__':
     print(
