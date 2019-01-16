ec02a7d38be0bc2aa267d1dbe579c58cbe696441
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-01-16 16:56:14 -0500

    - yaml converter - adjusting curve detection

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 with open('README.md') as readme_file:
     SETUP = {
         "name": "pdfmajor",
-        "version": "1.3.3",
+        "version": "1.3.4",
         "packages": find_packages(include=['pdfmajor*']),
         "install_requires": [
             'pycryptodome', 
