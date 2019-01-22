ac2c4625616f8964cb5e3b242417e294d79e103e
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-01-22 13:16:46 -0500

    - improved html converter

--- pdfmajor/__init__.py
+++ pdfmajor/__init__.py
@@ -97,7 +97,7 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-__version__ = (1, 3, 4)
+__version__ = (1, 3, 5)
 
 if __name__ == '__main__':
     print(
