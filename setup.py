bab104885f0f85f9507c43c494b1483f88717015
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-01-23 15:56:46 -0500

    - fixed caching error when running the interpreter multiple times on items with xobjects

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 with open('README.md') as readme_file:
     SETUP = {
         "name": "pdfmajor",
-        "version": "1.3.5",
+        "version": "1.3.6",
         "packages": find_packages(include=['pdfmajor*']),
         "install_requires": [
             'pycryptodome', 
